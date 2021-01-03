19a71fde7c29747281b7c04650e350b4e4cf8bad
Luis Martinez <luis_mmartinez@live.com>
2021-01-03 00:12:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,7 +94,7 @@ class TicTacToe
   def play
     turn until over?
     if won?
-      puts "Congratulations #{winner}"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
